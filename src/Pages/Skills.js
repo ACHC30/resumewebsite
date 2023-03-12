6c7217eb48425391bc8ac5d3cf63d55a8d16a7d5
Chris <chandrachristopher43@gmail.com>
2023-03-12 16:16:32 +1000

    change alignment foor the skills

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -3,30 +3,36 @@ function Skills() {
   return (
     <div>
       <Header />
-      <h1>Coding Skills</h1>
-      <ul>
-        <li>C#</li>
-        <li>PHP</li>
-        <li>HTML</li>
-        <li>CSS Languange</li>
-        <li>JavaScript</li>
-        <li>React.js</li>
-        <li>React Native</li>
-        <li>Expo</li>
-        <li>Express</li>
-      </ul>
-      <h1>Soft Skills</h1>
-      <ul>
-        <li>Scrum project management</li>
-        <li>Entity Relationship Diagram (ERD)</li>
-        <li>Object-oriented Programming</li>
-        <li>Data Structure</li>
-        <li>System Design and Implementation</li>
-        <li>Teamwork</li>
-        <li>Adaptability</li>
-        <li>Attention to Detail</li>
-        <li>Loyalty</li>
-      </ul>
+      <div className="skill-parent">
+        <div className="skill-child">
+          <h1>Coding Skills</h1>
+          <ul>
+            <li>C#</li>
+            <li>PHP</li>
+            <li>HTML</li>
+            <li>CSS Languange</li>
+            <li>JavaScript</li>
+            <li>React.js</li>
+            <li>React Native</li>
+            <li>Expo</li>
+            <li>Express</li>
+          </ul>
+        </div>
+        <div className="skill-child">
+          <h1>Soft Skills</h1>
+          <ul>
+            <li>Scrum project management</li>
+            <li>Entity Relationship Diagram (ERD)</li>
+            <li>Object-oriented Programming</li>
+            <li>Data Structure</li>
+            <li>System Design and Implementation</li>
+            <li>Teamwork</li>
+            <li>Adaptability</li>
+            <li>Attention to Detail</li>
+            <li>Loyalty</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
