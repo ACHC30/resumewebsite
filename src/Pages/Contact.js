0ff390f4b7df4dfb69cd1841a676287ffef785bc
Chris <chandrachristopher43@gmail.com>
2023-03-20 13:34:31 +1000

    updated the design for the contact details

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -10,14 +10,29 @@ function Contact() {
           <img src={myFace} className="App-logo-circular" alt="logo" />
         </div>
         <div style={{ width: "10%", display: "inline-block" }}></div>
-        <div className="skill-child" style={{ textAlign: "left" }}>
-          <ol>
-            <li>Address: Wolloonggabba, QLD 4102</li>
-            <li>Phone Number: 0432 644 477 OR (+62) 432 644 477</li>
-            <li>Linkedin: linkedin.com/in/christopher-chandra-9601bb1b9</li>
-            <li>Github Profile: https://github.com/ACHC30 </li>
-            <li>Email: chandrachristopher43@gmail.com</li>
-          </ol>
+        <div
+          className="skill-child"
+          style={{
+            background: "content-box radial-gradient(crimson, skyblue)",
+            borderRadius: "30px",
+          }}
+        >
+          <div style={{ margin: "10px" }}>
+            <h2>Address</h2>
+            <p>Wolloonggabba, QLD 4102</p>
+
+            <h2>Phone Number</h2>
+            <p>0432 644 477 OR (+62) 432 644 477</p>
+
+            <h2>Linkedin</h2>
+            <p>linkedin.com/in/christopher-chandra-9601bb1b9</p>
+
+            <h2>Github Profile</h2>
+            <p>https://github.com/ACHC30</p>
+
+            <h2>Email</h2>
+            <p>chandrachristopher43@gmail.com</p>
+          </div>
         </div>
       </div>
     </div>
