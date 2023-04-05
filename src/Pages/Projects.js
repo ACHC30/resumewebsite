85d6a534b023b49d670090f9e9c530940107cde0
Chris <chandrachristopher43@gmail.com>
2023-04-05 20:53:16 +1000

    plus h1

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,6 +1,10 @@
-
 function Projects() {
-  return <p>this page will provide links to all my previous projects</p>;
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>this page will provide links to all my previous projects</p>
+    </div>
+  );
 }
 
 export default Projects;
