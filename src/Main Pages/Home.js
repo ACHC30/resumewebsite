bb9b7c89cfa3c48342a37534aa82d9ebfe4abc9f
Chris <chandrachristopher43@gmail.com>
2023-08-08 17:23:32 +1000

    merged summary with home page

--- src/Main Pages/Home.js
+++ src/Main Pages/Home.js
@@ -4,6 +4,7 @@ import Popup from "../Components/Popup";
 import Header from "../Public Page/Header";
 import Footer from "../Public Page/Footer";
 import DownloadPDF from "../Components/DownloadPDF";
+import myFace from "../Images/MyFace.jpg";
 
 let hasUseEffectRun = false;
 
@@ -25,18 +26,26 @@ function Home() {
   return (
     <div className="App">
       <Popup isOpen={isPopupOpen} onClose={handleClosePopup} />
-
       <Header />
-      <h1 style={{ marginTop: "10%", margin: "5%" }}>
-        This website serves as a personal project I have developed with the
-        primary objective of honing and showcasing my coding skills.
-        Additionally, it also serves as a platform for potential recruiters to
-        assess my coding abilities. Going forward, I have plans to expand and
-        enhance its content further. I wholeheartedly welcome constructive
-        feedback from anyone, which can be directed to my email address provided
-        in the contact details section. Your valuable input is greatly
-        appreciated. Thank you for visiting!
-      </h1>
+
+      <div className="parent">
+        <div className="child">
+          <img src={myFace} className="App-logo-circular" alt="logo" />
+        </div>
+
+        <div className="child">
+          <h1>A bit about me</h1>
+          <p>
+            I am a software developer for both the back and front end of
+            development. Ever since I was a child I was always quiet, and I was
+            also fascinated by technology. Building high-quality software always
+            makes people's lives easier, which is why I love building software.
+            As a follow-up to my father's legacy, I aim to work in the IT
+            industry to do what I love and help others in the process as well.
+          </p>
+        </div>
+      </div>
+
       <DownloadPDF />
       <Footer />
     </div>
